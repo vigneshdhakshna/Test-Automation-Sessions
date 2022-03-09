9c5e2a533f9be01dd728a581169f00aaf7ea1ef9
VigneshD <vignesh.dhakshnamoorthy@cesltd.com>
2022-03-09 14:09:09 +0530

    git import

---
+++ src/test/java/testCASE/TestCaseDemo1.java
@@ -0,0 +1,31 @@
+package testCASE;
+
+import org.testng.annotations.Test;
+
+import config.TestNG.Author;
+import util.Base.TestBase;
+import util.Common.SeleniumUtil;
+
+
+public class TestCaseDemo1 extends TestBase{
+	
+	@Author(Name = "Vignesh")
+	@Test()
+	public void demo1TC() {
+		String webURL = "https://www.google.com/";
+		SeleniumUtil selenium = Selenium(webURL);
+		selenium.Log(selenium.getTitle());
+		selenium.pageScreenShot();
+}
+	
+	@Author(Name = "Vignesh")
+	@Test()
+	public void demo2TC() {
+		String webURL = "https://www.google.com/";
+		SeleniumUtil selenium = Selenium(webURL);
+		selenium.Log(selenium.getTitle());
+		selenium.pageScreenShot();
+
+}
+	
+}
