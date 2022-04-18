cb501c8ab81dad731ebe08e1497cf03ca5082b59
VigneshD <vignesh.dhakshnamoorthy@cesltd.com>
2022-04-18 18:35:54 +0530

    jar updated

--- src/test/java/testCASE/TestCaseDemo1.java
+++ src/test/java/testCASE/TestCaseDemo1.java
@@ -1,8 +1,10 @@
 package testCASE;
 
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 import config.TestNG.Author;
 import util.Base.TestBase;
@@ -14,25 +16,11 @@ public class TestCaseDemo1 extends TestBase{
 	@Author(Name = "Vignesh")
 	@Test()
 	public void demo1TC() {
-		String webURL = "https://www.valuedopinions.co.in/index";
+		String webURL = "https://money.rediff.com/gainers";
 		SeleniumUtil selenium = Selenium(webURL);
-		selenium.sleep(2);
-		selenium.findBy_Xpath("//a[@class='login button buttonTertiary']").click();
-		selenium.sleep(2);
-		selenium.findBy_ID("username").sendKeys("ssarantestuser@gmail.com");
-		selenium.findBy_ID("password").sendKeys("Test@123");
-		selenium.sleep(1);
-		selenium.findBy_Xpath("//input[@class='button buttonPrimary button-full']").click();
-		selenium.sleep(2);
-		
-		WebElement host = selenium.findBy_Xpath("//upp-punchcard-list[@ng-if='vm.user.id > 0']");
-		int num = 2;
-		SearchContext shadow0 = host.getShadowRoot();
-		SearchContext shadow1 = shadow0.findElement(By.cssSelector("upp-punchcard[show-modal='false']")).getShadowRoot();
-		WebElement findElement1 = shadow1.findElement(By.cssSelector("#pc-"+num));
-		selenium.Log(findElement1.getText());
-		WebElement findElement2 = shadow1.findElement(By.cssSelector("#pc-"+num+" > div"));
-		selenium.Log(findElement2.getCssValue("background-color"));
+		selenium.Log(selenium.getTitle());
+		List<WebElement> elements = selenium.getElements(By.xpath("//table[@class='dataTable']/tbody/tr"));
+		System.out.println(elements.size());
 	}
 	
 
--- src/test/java/testCASE/WebTest.java
+++ src/test/java/testCASE/WebTest.java
@@ -1,15 +1,24 @@
 package testCASE;
 
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.testproject.sdk.drivers.web.ChromeDriver;
 
 public final class WebTest {
 
     public static void main(final String[] args) throws Exception {
-        ChromeDriver driver = new ChromeDriver("4um1kZlnvPpB5R8Us9FmNZU9hNbL8uJMAn8pKgLjdQU1",new ChromeOptions());
-
-        driver.navigate().to("https://example.testproject.io/web/");
+    	WebDriverManager.firefoxdriver().setup();
+    	WebDriver driver = new FirefoxDriver();
+		
+    	driver.get("https://money.rediff.com/gainers");
+		List<WebElement> findElements = driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr"));
+		System.out.println(findElements.size());
 
 
         driver.quit();
