1f366ba9902f729cf84994bfaaaa048e3e5d6b05
VigneshD <vignesh.dhakshnamoorthy@cesltd.com>
2022-05-02 22:13:42 +0530

    fluentWait

--- src/test/java/testCASE/TestCaseDemo1.java
+++ src/test/java/testCASE/TestCaseDemo1.java
@@ -1,7 +1,20 @@
 package testCASE;
 
+import java.lang.reflect.Method;
 
-public class TestCaseDemo1 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import util.Base.TestBase;
+
+public class TestCaseDemo1 extends TestBase{
+	
+	
+	@Test
+	public void demoWEB() {
+
+	}
 	
 	
 
